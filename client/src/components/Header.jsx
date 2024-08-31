fd1b184153c601a0af94febe24dcd39fff1e7af0
Claire Lyons <clairelyons11@gmail.com>
2024-08-31 15:58:03 +0100

    Save current SPA version before starting mobile UI changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <header className="header">
         {/* Add logo, search bar, and menu items here */}
-      <div className="logo">Health Services Directory</div>
-      <button className="menu-toggle">☰</button>
+      <div className="logo">SláinteHub</div>
+      {/* <button className="menu-toggle">☰</button> */}
     </header>
   );
 };
